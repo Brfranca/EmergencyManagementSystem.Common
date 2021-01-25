1d74620551c04646ebcc81d0b9383ae24cab10bc
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-01-25 20:31:25 -0300

    Validação do Address finalizada.

---
+++ EmergencyManagementSystem.Common.BLL/BLL/AddressBLL.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using EmergencyManagementSystem.Common.BLL.Validations;
+using EmergencyManagementSystem.Common.Common.Models;
+using EmergencyManagementSystem.Common.DAL.DAL;
+using EmergencyManagementSystem.Common.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyManagementSystem.Common.BLL.BLL
+{
+    public class AddressBLL
+    {
+        private readonly IMapper _mapper;
+        private readonly AddressDAL _addressDAL;
+        private readonly AddressValidation _addressValidation;
+
+        public AddressBLL(AddressDAL addressDAL, AddressValidation addressValidation, IMapper mapper)
+        {
+            _addressDAL = addressDAL;
+            _addressValidation = addressValidation;
+            _mapper = mapper;
+        }
+
+        public Result Register(AddressModel addressModel)
+        {
+            try
+            {
+                var address = _mapper.Map<Address>(addressModel);
+
+                var result = _addressValidation.Validate(address);
+                if (!result.Success)
+                    return result;
+
+                _addressDAL.Insert(address);
+                return _addressDAL.Save();
+            }
+            catch (Exception error)
+            {
+                return Result.BuildError("Erro no momento de registar o endereço.", error);
+            }
+        }
+    }
+}
--- EmergencyManagementSystem.Common.BLL/Validations/AddressValidation.cs
+++ EmergencyManagementSystem.Common.BLL/Validations/AddressValidation.cs
@@ -20,8 +20,10 @@ namespace EmergencyManagementSystem.Common.BLL.Validations
 
             RuleFor(e => e.CEP)
                 .NotNull()
-                .MaximumLength(8)
-                .WithMessage("Favor inserir o CEP");
+                .NotEmpty()
+                .WithMessage("Favor inserir o CEP")
+                .Length(8)
+                .WithMessage("O CEP deve conter 8 caracteres.");
 
             RuleFor(e => e.City)
                 .NotNull()
@@ -33,13 +35,36 @@ namespace EmergencyManagementSystem.Common.BLL.Validations
                 .WithMessage("Nome da cidade não deve conter números e caracteres especiais.");
 
             RuleFor(e => e.District)
-                .NotNull()
-                .WithMessage("O bairro deve ser informado.")
                 .Length(3, 60)
                 .WithMessage("O bairro deve ter entre 3 e 60 letras.")
                 .Must(IsValidName)
                 .WithMessage("Nome do bairro não deve conter números e caracteres especiais.");
 
+            RuleFor(e => e.Complement)
+                .Length(3, 60)
+                .WithMessage("O complemento deve ter entre 3 e 60 letras.");
+
+            RuleFor(e => e.Number)
+                .Length(3, 10)
+                .WithMessage("O número deve ter entre 3 e 10 letras.");
+
+            RuleFor(e => e.Reference)
+                .Length(3, 60)
+                .WithMessage("A referência deve ter entre 3 e 60 letras");
+
+            RuleFor(e => e.State)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("O estado deve ser informado.")
+                .Length(2)
+                .WithMessage("O estado deve conter 2 letras.");
+
+            RuleFor(e => e.Street)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("A rua deve ser informada.")
+                .Length(3, 60)
+                .WithMessage("A rua deve conter entre 3 e 60 letras.");
 
         }
 
