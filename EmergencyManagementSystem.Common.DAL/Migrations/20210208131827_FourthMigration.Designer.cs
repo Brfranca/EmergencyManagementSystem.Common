d0e9b6763520df22180ecf6ef277fb91464a99f9
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-08 10:20:52 -0300

    feat: fourth migration

--- EmergencyManagementSystem.Common.DAL/Migrations/20210208131827_FourthMigration.Designer.cs
+++ EmergencyManagementSystem.Common.DAL/Migrations/20210208131827_FourthMigration.Designer.cs
@@ -4,14 +4,16 @@ using EmergencyManagementSystem.Common.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmergencyManagementSystem.Common.DAL.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210208131827_FourthMigration")]
+    partial class FourthMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
